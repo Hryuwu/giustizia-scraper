9786092b88c517a36731a7d2c5a7a78ef69bb5b0
Hryuwu <christiancaruso97@gmail.com>
2025-08-13 12:11:16 +0200

    aaaaaaaaaaaaaaaaaaaaaaa

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     # socketio.run chooses the best async mode (eventlet/gevent/threading).
     # On Railway, add 'eventlet' to requirements and it'll take it.
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
